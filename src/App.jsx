046ce9bd15599d6779cae1bdda56f305d70da6e7
Nexus <wv35788@gmail.com>
2024-07-03 01:24:19 -0500

    Setup 🥶

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <button className='bg-slate-200 px-4 py-2 rounded hover:bg-slate-300'>
+            App
+      </button>
+    </div>
+  )
+}
+
+export default App
